ea6ef2401ba5c504a44d3a252c0b5e7a26d55044
Emma Gilmour <e.gilmour33@gmail.com>
2018-11-01 18:10:31 +0000

    improve code for displaying message

--- App.js
+++ App.js
@@ -4,19 +4,40 @@ import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class App extends React.Component {
   state = {
-    messages: [
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date()
-      }
-    ]
+    messages: []
   };
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any'
+          }
+        }
+      ]
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <GiftedChat messages={this.state.messages} />
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{ _id: 1 }}
+        />
       </View>
     );
   }
